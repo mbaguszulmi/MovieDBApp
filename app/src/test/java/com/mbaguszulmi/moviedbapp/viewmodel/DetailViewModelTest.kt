69eed62c402c189d8079188e9375be6adb18d90b
mbaguszulmi <baguszulmi101198@gmail.com>
2021-05-26 08:46:51 +0700

    test(DetailViewModelTest): check all properties in movie or TV detail

--- app/src/test/java/com/mbaguszulmi/moviedbapp/viewmodel/DetailViewModelTest.kt
+++ app/src/test/java/com/mbaguszulmi/moviedbapp/viewmodel/DetailViewModelTest.kt
@@ -2,6 +2,9 @@ package com.mbaguszulmi.moviedbapp.viewmodel
 
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.mbaguszulmi.moviedbapp.model.database.entities.Genre
+import com.mbaguszulmi.moviedbapp.model.network.Movie
+import com.mbaguszulmi.moviedbapp.model.network.TV
 import junit.framework.TestCase
 import org.junit.Before
 import org.junit.Rule
@@ -32,6 +35,17 @@ class DetailViewModelTest : TestCase() {
     @Test
     fun testGetMovie() {
         val expectedMovieId = 578701
+        val expectedMovie = Movie(expectedMovieId, false,
+            "/iDdpiUnCeXvBmrkBFpL6lKsZt33.jpg", listOf(Genre(id=53, name="Thriller"),
+                Genre(id=18, name="Drama"), Genre(id=28, name="Action"),
+                Genre(id=9648, name="Mystery")),
+            "https://www.warnerbros.com/movies/those-who-wish-me-dead", "tt3215824",
+            "en", "Those Who Wish Me Dead", "A young boy finds " +
+                    "himself pursued by two assassins in the Montana wilderness with a survival " +
+                    "expert determined to protecting him - and a forest fire threatening to consume " +
+                    "them all.", 2406.716, "/xCEg6KowNISWvMh8GvPSxtdf9TO.jpg",
+            "2021-05-05", 100, "Released", "Nature finds a way.",
+            "Those Who Wish Me Dead", 7.1, 279)
 
         detailViewModel.getMovie(instrumentationContext, expectedMovieId) {
             // Do Nothing on failure
@@ -41,14 +55,26 @@ class DetailViewModelTest : TestCase() {
         Thread.sleep(5000)
 
         val fetchedMovie = detailViewModel.movieValue
+
+        // check if fetchedMovie is not null
         assertNotNull(fetchedMovie)
-        // check if fetchedMovie.id = 578701
-        assertEquals(fetchedMovie?.id, expectedMovieId)
+        // check all property in fetchedMovie
+        assertEquals(fetchedMovie, expectedMovie)
     }
 
     @Test
     fun testGetTV() {
         val expectedTVId = 120168
+        val expectedTV = TV(expectedTVId, "/dYvIUzdh6TUv4IFRq8UBkX7bNNu.jpg",
+            listOf(Genre(id=18, name="Drama"), Genre(id=80, name="Crime"),
+                Genre(id=9648, name="Mystery")),
+            "https://www.netflix.com/title/81166747", true,
+            "2021-05-19", "Who Killed Sara?", 18, 2,
+            "es", "¿Quién mató a Sara?", "Hell-bent on exacting " +
+                    "revenge and proving he was framed for his sister's murder, Álex sets out " +
+                    "to unearth much more than the crime's real culprit.",
+            1257.949, "/o7uk5ChRt3quPIv8PcvPfzyXdMw.jpg", "Returning Series",
+            "", "Scripted", 7.8, 704)
 
         detailViewModel.getTV(instrumentationContext, expectedTVId) {
             // Do Nothing on failure
@@ -58,8 +84,10 @@ class DetailViewModelTest : TestCase() {
         Thread.sleep(5000)
 
         val fetchedTV = detailViewModel.tvValue
+
+        // check if fetchedTV is not null
         assertNotNull(fetchedTV)
-        // check if fetchedTV.id = 120168
-        assertEquals(fetchedTV?.id, expectedTVId)
+        // check all property in fetchedTV
+        assertEquals(fetchedTV, expectedTV)
     }
 }
